29c17016f7c437c892f9d7f30aeaa2437c4c84b4
kostyanf14 <kostyanf14@live.com>
2018-12-24 01:34:28 +0200

    Changer template. Build table very slow.

--- app/web/web.py
+++ app/web/web.py
@@ -7,16 +7,33 @@ from ..models import *
 
 class Web:
     def __init__(self) -> None:
-        self.index_template = Environment(loader=PackageLoader('app.web', '')).get_template('index.html')
+        self.index_template = Environment(loader=PackageLoader(
+            'app.web', '')).get_template('index.html')
         self.db = Database()
 
     @expose
     def index(self) -> str:
-        problems = Problem.all(self.db)
         users = User.all(self.db)
+
+        problems = Problem.all(self.db)
+        p = dict()
+        g = set()
+        for problem in problems:
+            if problem.group in p:
+                p[problem.group].append(problem.id)
+            else:
+                p[problem.group] = [problem.id]
+            g.add(problem.group)
+
         submissions = Submission.all(self.db)
-        return self.index_template.render(problems=problems, users=users, submissions=submissions)
+        s = dict()
+        for submission in submissions:
+            s[(submission.user_id, submission.problem_id)] = (
+                submission.id, submission.score)
+
+        return self.index_template.render(groups=g, problems=p, users=users, submissions=s)
 
     def start(self) -> None:
-        config.update({'server.socket_host': environ['WEB_HOST'], 'server.socket_port': int(environ['WEB_PORT'])})
+        config.update({'server.socket_host': environ['WEB_HOST'], 'server.socket_port': int(
+            environ['WEB_PORT'])})
         quickstart(self)
