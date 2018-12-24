a2d1a583e78f8ed414eb3798c6dc10af6c5a1379
Just Me <just806me@gmail.com>
2018-12-24 13:45:59 +0200

    refactored web index a bit

--- app/web/web.py
+++ app/web/web.py
@@ -1,6 +1,8 @@
 from os import environ
 from cherrypy import expose, quickstart, config
 from jinja2 import Environment, PackageLoader
+import itertools
+
 from ..database import Database
 from ..models import *
 
@@ -13,23 +15,10 @@ class Web:
     @expose
     def index(self) -> str:
         users = User.all(self.db)
-
-        problems = Problem.all(self.db)
-        p = dict()
-        g = set()
-        for problem in problems:
-            if problem.group in p:
-                p[problem.group].append(problem.id)
-            else:
-                p[problem.group] = [problem.id]
-            g.add(problem.group)
-
-        submissions = Submission.all(self.db)
-        s = dict()
-        for submission in submissions:
-            s[(submission.user_id, submission.problem_id)] = (submission.id, submission.score)
-
-        return self.index_template.render(groups=g, problems=p, users=users, submissions=s)
+        problems = {k: list(v) for k, v in itertools.groupby(Problem.all(self.db), lambda p: p.group)}
+        groups = list(problems.keys())
+        submissions = {(s.user_id, s.problem_id): (s.id, s.score) for s in Submission.all(self.db)}
+        return self.index_template.render(groups=groups, problems=problems, users=users, submissions=submissions)
 
     def start(self) -> None:
         config.update({'server.socket_host': environ['WEB_HOST'], 'server.socket_port': int(environ['WEB_PORT'])})
