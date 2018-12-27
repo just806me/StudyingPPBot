5d7c244c5c15d43f414d75978ed45a4be075bf54
kostyanf14 <kostyanf14@live.com>
2018-12-27 20:22:50 +0200

    Fixed ukranian text

--- app/bot/resources.py
+++ app/bot/resources.py
@@ -53,4 +53,4 @@ CREATE_SUBMISSION_ERROR_USERNAME_INVALID = "Твій нік (%s) не співп
 
 CREATE_SUBMISSION_ERROR_SUBMISSION_EXISTS = "Розв'язок з ІД %s вже додано."
 
-RESULTS_TEXT = "Гарні таблички з твоїми балами та списком задач можна знайти на сайті http://studying.codelabs.site/. Быльшість чисел це посилання, щоб було легше та швидше шукати"
+RESULTS_TEXT = "Гарні таблички з твоїми балами та списком задач можна знайти на сайті http://studying.codelabs.site/. Більшість чисел це посилання, щоб було легше та швидше шукати"
